d7f1cf4b238ae73ffae25c7d57c47dc2b7d2b0ea
st-lana <st.lana@hotmail.com>
2021-11-29 21:13:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int currentBalance = 2_000_000_000;
+        int orderSum = 500_000_000;
+        int totalSum = currentBalance + orderSum;
+
+        System.out.println(totalSum);
+    }
+}
